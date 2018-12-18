a491ed64e21794594c1f86349d7d4589a6bbaa62
Anuitex <Anuitex@DESKTOP-FO8BPCL>
2018-12-18 12:24:31 +0200

    Adding Autorization

---
+++ WebApplication1/Validators/CustomUserValidator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Validators
+{
+    
+    public class CustomUserValidator : IUserValidator<IdentityUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<IdentityUser> manager, IdentityUser user)
+        {
+            List<IdentityError> errors = new List<IdentityError>();
+                       
+            
+            return Task.FromResult(errors.Count == 0 ?
+                IdentityResult.Success : IdentityResult.Failed(errors.ToArray()));
+        }
+    }
+}
