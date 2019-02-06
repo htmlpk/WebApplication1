75608e2bbf35ddfe6c99dc96d496e5e60e21325b
Anuitex <Anuitex@DESKTOP-FO8BPCL>
2019-02-06 11:28:15 +0200

    fixed dugs

--- UI/ClientApp/src/app/app.module.ts
+++ UI/ClientApp/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { LoginComponent } from 'src/app/login/login.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppComponent } from 'src/app/app.component';
 import { ErrorComponent } from 'src/app/error/error.component';
 import { GameComponent } from 'src/app/game/game.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AppComponent } from 'src/app/app.component';
-import { RouterModule,Routes} from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http'
-import { ParamInterceptor } from 'src/app/shared/services/param.interceptor';
+import { LoginComponent } from 'src/app/login/login.component';
 import { GameService } from 'src/app/shared/services/game.service';
-import { AppRoutingModule } from 'src/app/app-routing.module';
+import { ParamInterceptor } from 'src/app/shared/services/param.interceptor';
 
 @NgModule({
   declarations: [
@@ -35,5 +35,3 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
